17961644458fb52008d1f9d1a6c1168f96b4cdac
NicolasMosqueda <Nicomosqueda02@gmail.com>
2023-05-30 09:42:27 -0700

    hi

--- _includes/scrpt.js
+++ _includes/scrpt.js
@@ -1,4 +1,5 @@
-function showLebronImage() {
+function showLebronImage(event) {
+    event.preventDefault();
     var lebronImage = document.getElementById("lebron-image");
     lebronImage.style.display = "block";
   }
